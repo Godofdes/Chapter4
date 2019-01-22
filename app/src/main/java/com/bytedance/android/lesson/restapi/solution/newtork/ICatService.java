cf22749b54b1a9adbb8f4475db6b4d94acee288f
Godofdes <34367123+Godofdes@users.noreply.github.com>
2019-01-22 23:37:35 +0800

    Update ICatService.java

--- app/src/main/java/com/bytedance/android/lesson/restapi/solution/newtork/ICatService.java
+++ app/src/main/java/com/bytedance/android/lesson/restapi/solution/newtork/ICatService.java
@@ -6,5 +6,7 @@ package com.bytedance.android.lesson.restapi.solution.newtork;
  * @date 2019.01.15 16:42
  */
 public interface ICatService {
+    @GET("v1/images/search")
+    Call<List<Cat>>randomCat(@Query("limit") int l);
     // TODO-C1 (2) Implement your Cat Request here, url: https://api.thecatapi.com/v1/images/search?limit=5
 }
