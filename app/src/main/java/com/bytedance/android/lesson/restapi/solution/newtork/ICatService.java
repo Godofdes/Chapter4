0878e0736af80084810c35fcaee22bee47018abb
Godofdes <34367123+Godofdes@users.noreply.github.com>
2019-01-22 23:40:39 +0800

    Update ICatService.java

--- app/src/main/java/com/bytedance/android/lesson/restapi/solution/newtork/ICatService.java
+++ app/src/main/java/com/bytedance/android/lesson/restapi/solution/newtork/ICatService.java
@@ -1,6 +1,12 @@
 package com.bytedance.android.lesson.restapi.solution.newtork;
 
+import com.bytedance.android.lesson.restapi.solution.bean.Cat;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
 /**
  * @author Xavier.S
  * @date 2019.01.15 16:42
