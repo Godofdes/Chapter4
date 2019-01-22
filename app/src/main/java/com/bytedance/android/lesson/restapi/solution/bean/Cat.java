e5a4c3b25007ba4093d43dfd0060f39b1aea4d67
Godofdes <34367123+Godofdes@users.noreply.github.com>
2019-01-22 23:39:55 +0800

    Update Cat.java

--- app/src/main/java/com/bytedance/android/lesson/restapi/solution/bean/Cat.java
+++ app/src/main/java/com/bytedance/android/lesson/restapi/solution/bean/Cat.java
@@ -1,10 +1,36 @@
 package com.bytedance.android.lesson.restapi.solution.bean;
 
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
 /**
  * @author Xavier.S
  * @date 2019.01.17 18:08
  */
 public class Cat {
+        private List<String> breeds;
+        private String id;
+        private String url;
+        public void setBreeds(List<String> breeds) {
+            this.breeds = breeds;
+        }
+        public List<String> getBreeds() {
+            return breeds;
+        }
 
+        public void setId(String id) {
+            this.id = id;
+        }
+        public String getId() {
+            return id;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+        public String getUrl() {
+            return url;
+        }
     // TODO-C1 (1) Implement your Cat Bean here according to the response json
 }
